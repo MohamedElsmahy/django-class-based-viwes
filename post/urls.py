413df76370542174fa821f00578be22af64d8f38
MohamedElsmahy <66690245+MohamedElsmahy@users.noreply.github.com>
2020-12-04 17:39:50 +0200

    add class based views structure

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('', views.post_list , name = 'post_list'),
-    path('<int:id>', views.post_detail , name = 'post_detail'),
+    path('', views.PostList.as_view(), name = 'post_list'),
+    path('<int:pk>', views.PostDetail.as_view() , name = 'post_detail'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.urls import reverse
+from .models import Post
+from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
 
 # Create your views here.
 
-def post_list(request):
+#Function Views
+
+''' def post_list(request):
+    post_list = Post.objects.all()
+    return render(request , 'post/list.html' , {'post_list' : post_list})
+ '''
+
+''' def post_detail(request , id):
+    pass '''
+
+#Class Based Views
+
+class PostList(ListView):
+    model = Post 
+
+
+class PostDetail(DetailView):
+    model = Post
+
+class PostCreate(CreateView):
     pass
 
-
-def post_detail(request):
+class PostDelete(DeleteView):
     pass
